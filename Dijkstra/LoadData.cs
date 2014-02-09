6b52830541e12fee8c50622566e80d6c8f6992a1
Безуглов Сергей <Besuglov.S@Gmail.com>
2014-02-09 13:18:50 +0400

    Test (+Empty line)

--- Dijkstra/LoadData.cs
+++ Dijkstra/LoadData.cs
@@ -24,6 +24,7 @@ namespace Dijkstra
 
                 edges.Add(new Edge(edgeStart, edgeEnd, edgeWeight));
             }
+
             sr.Close();
 
             return new GraphData(rank, edges);
